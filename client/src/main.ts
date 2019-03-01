a4a4096f5d4f027d11ba933bb668a91e7d2664b0
Sam Julien <spjulien@gmail.com>
2019-02-28 17:06:04 -0800

    Initial commit

---
+++ client/src/main.ts
@@ -0,0 +1,17 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { setAngularJSGlobal } from '@angular/upgrade/static';
+import * as angular from 'angular';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+setAngularJSGlobal(angular);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
