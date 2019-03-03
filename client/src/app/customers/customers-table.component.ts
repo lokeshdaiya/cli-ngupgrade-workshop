ec559512dd15d489e9380cd723d2504da9ae3ac0
Sam Julien <spjulien@gmail.com>
2019-03-03 11:48:02 -0800

    Use async pipe with customers

--- client/src/app/customers/customers-table.component.ts
+++ client/src/app/customers/customers-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Customer } from './customer.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'customers-table',
   templateUrl: './customers-table.component.html'
 })
 export class CustomersTableComponent {
-  @Input() customers: Customer[];
+  @Input() customers: Observable<Customer[]>;
   constructor() {}
 }
--- client/src/app/customers/customers.component.ts
+++ client/src/app/customers/customers.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Customer } from './customer.interface';
 import { CustomerService } from './customer.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'customers',
   templateUrl: './customers.component.html'
 })
-export class CustomersComponent implements OnInit {
-  customers: Customer[];
+export class CustomersComponent {
+  customers: Observable<Customer[]> = this.customerService.getCustomers();
   title = 'Customers';
   constructor(private customerService: CustomerService) {}
-
-  ngOnInit(): void {
-    this.customerService.getCustomers().subscribe(data => {
-      this.customers = data;
-    });
-  }
 }
